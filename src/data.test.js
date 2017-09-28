5cf6631b2848167bc340435386d601957d367601
Chris Getsfred <c_getsfred@tcdi.com>
2017-09-28 17:13:42 -0500

    add comments

--- src/data.test.js
+++ src/data.test.js
@@ -3,7 +3,9 @@ import fetch from 'node-fetch';
 import { getCategories } from './data';
 
 beforeEach(() => {
-  jest.resetAllMocks();
+  // Reset the modules back to their original state.
+  // In this case, we are setting `node-fetch` back
+  // to the original, unmocked version
   jest.resetModules();
 });
 
@@ -28,14 +30,56 @@ describe('getCategories', () => {
       ],
     };
 
+    // Here we are telling jest to mock `node-fetch` with the
+    // specified factory method. The factory method creates
+    // a mock function, which returns the same implementation
+    // as when the files were separate
     jest.doMock('node-fetch', () => {
-      return jest.fn(async () => ({
-        json: async () => mockResponse,
-      }));
+      return jest.fn(async () => {
+        return { json: async () => mockResponse };
+      });
     });
+
+    // We now have to re-import the module we are testing
     const data = require('./data');
 
+    // Make the call to the newly imported module
     const categories = await data.getCategories();
+
+    expect(categories).toEqual([
+      { id: 1, name: 'Category 1' },
+      { id: 2, name: 'Category 2' },
+      { id: 3, name: 'Category 3' },
+      { id: 4, name: 'Category 4' },
+    ]);
+  });
+
+  it('should return expected mock data', async () => {
+    const mockResponse = {
+      categories: [
+        { catId: 1, catName: 'Category 1' },
+        { catId: 2, catName: 'Category 2' },
+        { catId: 3, catName: 'Category 3' },
+        { catId: 4, catName: 'Category 4' },
+      ],
+    };
+
+    // Here we are telling jest to mock `node-fetch` with the
+    // specified factory method. The factory method creates
+    // a mock function, which returns the same implementation
+    // as when the files were separate
+    jest.doMock('node-fetch', () => {
+      return jest.fn(async () => {
+        return { json: async () => mockResponse };
+      });
+    });
+
+    // We now have to re-import the module we are testing
+    const data = require('./data');
+
+    // Make the call to the newly imported module
+    const categories = await data.getCategories();
+
     expect(categories).toEqual([
       { id: 1, name: 'Category 1' },
       { id: 2, name: 'Category 2' },
