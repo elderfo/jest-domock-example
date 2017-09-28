e7d93abc51da649f2d7fb16bad81aab3b3f9de6c
Chris Getsfred <c_getsfred@tcdi.com>
2017-09-28 16:46:47 -0500

    2-combined-tests

--- src/data.test.js
+++ src/data.test.js
@@ -9,6 +9,16 @@ beforeEach(() => {
 });
 
 describe('getCategories', () => {
+  it('should return expected data from server', async () => {
+    const categories = await getCategories();
+    expect(categories).toEqual([
+      { id: 1, name: 'Category 1' },
+      { id: 2, name: 'Category 2' },
+      { id: 3, name: 'Category 3' },
+      { id: 4, name: 'Category 4' },
+    ]);
+  });
+
   it('should return expected mock data', async () => {
     const mockResponse = {
       categories: [
